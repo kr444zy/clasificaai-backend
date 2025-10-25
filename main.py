5d01f62b8806ed37fd9e170e7a9f76ded4595a31
kr444zy <76142404+kr444zy@users.noreply.github.com>
2025-10-25 00:53:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI(title="ClasificaAI Backend")
 
+from ClasificaAI-IA-Base.ai_service import router as ai_router
+app.include_router(ai_router, prefix="/ai")
+
 @app.get("/")
 def root():
     return {"msg": "Backend ClasificaAI funcionando"}
